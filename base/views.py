eb1141e9ba3299f4501cd0d8e607e480baf474bb
SamirMamede <anuarsamir@gmail.com>
2023-08-01 21:59:57 -0300

    set templates dir

--- base/views.py
+++ base/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Home page')
+    return render(request, 'home.html')
 
 def room(request):
-    return HttpResponse('ROOM')
+    return render(request, 'room.html')

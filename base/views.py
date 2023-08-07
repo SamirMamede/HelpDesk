cd7342819d26eed8a7b7213c5ada54281e4fd42b
SamirMamede <anuarsamir@gmail.com>
2023-08-06 23:26:24 -0300

    home, room, main, navbar templates.

--- base/views.py
+++ base/views.py
@@ -2,9 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+rooms = [
+    
+        {'id': 1, 'name': 'ATX ONE'},
+        {'id': 2, 'name': 'ATX EMISSOR'},
+        {'id': 3, 'name': 'ATX SALES'},
+    
+]
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room= None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
